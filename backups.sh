360489f261e85196b58e4b7e3801743001cad249
Enrique <eramirez@wmtechnologies.cl>
2023-08-10 02:34:47 -0400

    modulacion

--- backups.sh
+++ backups.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
-# Carpeta de destino para los respaldos
-backup_dir="/ruta/destino/backups"
+# Define the Docker Compose directory and file
+compose_dir="./HomeServiceDocker"
+compose_file="docker-compose.yml"
+
+# Change to project directory
+cd "$compose_dir"
+
+# Destination folder for the backups
+backup_dir="./backups"
 timestamp=$(date +"%Y%m%d%H%M%S")
 backup_file="backup_$timestamp.tar.gz"
 
-# Carpetas a respaldar
+# Folders to backup
 folders=("envs" "data" "config")
 
-# Crear carpeta de destino si no existe
+# Stop the Docker Compose services
+echo "Stopping Docker Compose services..."
+docker-compose -f "$compose_file" down
+
+# Create the destination folder if it doesn't exist
 mkdir -p "$backup_dir"
 
-# Generar el respaldo
+# Generate the backup
 tar -czvf "$backup_dir/$backup_file" "${folders[@]}"
 
-# Verificar si el respaldo fue exitoso
+# Check if the backup was successful
 if [ $? -eq 0 ]; then
-  echo "Respaldo generado correctamente: $backup_dir/$backup_file"
+  echo "Backup successfully generated: $backup_dir/$backup_file"
+  
+  # Upload the backup to Google Drive
+  echo "Uploading backup to Google Drive..."
+  rclone copy --update "$backup_dir/$backup_file" "gdrive:backups"
+
+  if [ $? -eq 0 ]; then
+    echo "Backup successfully uploaded to Google Drive."
+  else
+    echo "Error while uploading backup to Google Drive."
+  fi
 else
-  echo "Error al generar el respaldo."
+  echo "Error while generating the backup."
 fi
+
+# Always restart the Docker Compose services at the end
+echo "Restarting Docker Compose services..."
+docker-compose -f "$compose_file" up -d
